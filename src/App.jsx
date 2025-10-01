c53a4cf87efc5f9eb3cfdf65a4b6c2a3341f4e7e
moaaz-cse <moaazcse@gmail.com>
2025-10-01 12:14:25 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
       <Experience />
       <Services />
       <Experience />
+      <ContactForm />
       <Project />
       <SkillsSection />
       <ContactForm />
