5d31c639c309ec3a95d5b9e0bd061cc46717c8fc
moaaz-cse <moaazcse@gmail.com>
2025-10-30 15:36:15 +0530

    merging code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "text-blue-500",
+    "text-yellow-400",
+    "text-blue-400",
+    "text-cyan-400",
+    "text-green-500",
+    "text-gray-300",
+    "text-green-600",
+    "text-blue-600",
+    "text-orange-400",
+    "text-red-500",
+  ],
   theme: {
     extend: {
       fontFamily: {
